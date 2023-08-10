56d66d4b717c44d40b8d7ce9b0609919ae5b0201
islamurmatbekuulu <itokoev00@gmail.com>
2023-08-10 18:34:52 +0300

    kenar belirleme 2
    Bu örnekte laplacian metodu kullanılmıştır.

---
+++ kenar belirleme 2 laplacian.py
@@ -0,0 +1,18 @@
+import cv2
+import matplotlib.pyplot as plt
+
+# Görüntüyü yükle
+image = cv2.imread('image1.jpg', cv2.IMREAD_COLOR)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Laplacian kenar dedektörü
+laplacian = cv2.Laplacian(gray, cv2.CV_64F)
+
+# Sonuçları göster
+plt.imshow(laplacian, cmap='gray')
+plt.title('Laplacian Kenar Dedektörü')
+plt.axis('off')
+
+# Çıktıyı kaydet
+output_filename = 'edges_output2.jpg'
+cv2.imwrite(output_filename, laplacian)
